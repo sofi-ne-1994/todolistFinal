b597d3f78d627a5bd6782bc832ff34f4a80a6ef0
sofi-ne-1994 <sofia.bellatti@nuevaescuela.net>
2025-11-27 20:18:07 -0300

    Agregue desarrollo al about

--- todolistfront/src/pages/About.jsx
+++ todolistfront/src/pages/About.jsx
@@ -4,7 +4,10 @@ export default function About (){
             <div className="main-card">
               <h1>Sobre Mi</h1>
               <img src="" alt="" />
-              <p>Hola, me soy Sofi, aspirante a desarolladora.  que hice con React y Vite  y para el backend use Django y python.</p>
+              <p>Hola, soy Sofi, aspirante a desarrolladora y fan de las apps simples que realmente te salvan el día. Esta To-Do List la hice como proyecto final de PWA. 
+              Acá vas a poder crear tareas nuevas, ver todo lo que tenés pendiente, marcar lo que ya completaste y borrar lo que ya no sirve. Todo rapidito, práctico y sin vueltas.
+              Desarrollada con React+ Vite, Django y Python C:
+              Gracias por pasar y probar mi app!</p>
             </div>
         </div>
     );
