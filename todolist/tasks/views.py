111bb26dd9f1efe3397930f0a6bd8bc5483b4e95
sofi-ne-1994 <sofia.bellatti@nuevaescuela.net>
2025-11-28 19:11:36 -0300

    agregue comentarios

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -3,7 +3,14 @@ from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
 
-
+# Usamos ModelViewSet para implementar todas las consignas del CRUD:
+# - Crear (POST)
+# - Ver todas (GET list)
+# - Eliminar (DELETE)
+# - Actualizar (PUT/PATCH - para marcar como completada)
 class TaskViewSet(viewsets.ModelViewSet):
+    # La lista completa de objetos (Ver todas las tareas)
     queryset = Task.objects.all()
+    # El serializador que usaremos
     serializer_class = TaskSerializer
+    # ModelViewSet maneja el GET, POST, DELETE, etc., automáticamente.
